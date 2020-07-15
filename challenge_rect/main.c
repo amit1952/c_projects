4a6f6c4e1be2013c6191a62af12157750229bb45
Amitava Das Gupta <amitavadg@gmail.com>
2020-07-15 11:27:02 +0530

    Added to git. Hard-coded height and width values. TODO: Ask for height and width values.

---
+++ challenge_rect/main.c
@@ -0,0 +1,26 @@
+/* Program to calculate the perimeter and
+ * area of a rectangle. Width and height are
+ * supplied
+ */
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    // Declare and Initialize variables
+    double height, width, area, perimeter;
+    height = 10.25;
+    width = 30.5;
+    area = 0.0;
+    perimeter = 0.0;
+
+    // Calculate values
+    perimeter = (height+width) * 2;
+    area = width * height;
+
+    // Print results
+    printf("The height of the rectangle is %.2f\nThe width of the rectangle is %.2f\n", height, width);
+    printf("The perimeer of the rectangle is: %.2f\n", perimeter);
+    printf("The area of the rectangle is: %.2f\n", area);
+    return 0;
+}
+
