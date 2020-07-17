730f4a770dae80b797dcfc6ea3ef07a2289cd577
Amitava Das Gupta <amitavadg@gmail.com>
2020-07-17 09:52:41 +0530

    Addition.

---
+++ ops/test.c
@@ -0,0 +1,16 @@
+/* Testing operators */
+#include <stdio.h>
+
+int main() {
+    int a = 33;
+    int b = 15;
+    int result = 0;
+
+    result = a + b;
+    
+    printf("a = %d, b = %d\n", a, b);
+    printf("%d + %d = %d\n", a, b, result);
+
+    return 0;
+}
+
