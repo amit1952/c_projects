565ccb38f58687fbff2cb50d0e0001faeb7c86b7
Amitava Das Gupta <amitavadg@gmail.com>
2020-07-18 10:20:23 +0530

    Unary prefix and postfix operators.

--- ops/pfix.c
+++ ops/pfix.c
@@ -3,13 +3,24 @@
 #include <stdio.h>
 
 int main() {
+    printf("\nUNARY OPERATORS - POSITIVE\n");
     int a = 33;
     printf("The Postfix Operator\na = %d\n", a);
     printf("The result of a++ = %d\n", a++);
 
     a = 33;
-    printf("The Prefix Operator\na = %d\n", a);
+    printf("\nThe Prefix Operator\na = %d\n", a);
     printf("The result of ++a = %d\n", ++a);
+    
+    printf("\nUNARY OPERATORS - NEGATIVE\n");
+    a = 33;
+    printf("The Postfix Operator\na = %d\n", a);
+    printf("The result of a-- = %d\n", a--);
+
+    a = 33;
+    printf("\nThe Prefix Operator\na = %d\n", a);
+    printf("The result of --a = %d\n", --a);
     return 0;
+    
 }
 
