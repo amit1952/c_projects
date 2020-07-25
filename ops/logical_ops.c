ddd9e6045f7d4f56b72b0b4ac6aee722bcbc711d
Amitava Das Gupta <amitavadg@gmail.com>
2020-07-25 10:25:53 +0530

    Clean up code.

--- ops/logical_ops.c
+++ ops/logical_ops.c
@@ -1,18 +1,79 @@
 /*
- * Creator Amitava Das Gupta
- * Date:
+ * Creator: Amitava Das Gupta
+ * Date: 20/07/2010
  */
 #include <stdio.h>
 #include <stdbool.h>
 
 int main() {
+    printf("OR (||) Operations.\n");
+
     _Bool a = true;
     _Bool b = false;
     _Bool result;
 
+    result = a || b;
+
+    printf("a = %d, b = %d\n", a, b);
+    printf("The result of a || b = %d\n", result);
+    
+    a = true;
+    b = true;
+
+    result = a || b;
+
+    printf("a = %d, b = %d\n", a, b);
+    printf("The result of a || b = %d\n", result);
+
+    a = false;
+    b = false;
+
+    result = a || b;
+
+    printf("a = %d, b = %d\n", a, b);
+    printf("The result of a || b = %d\n", result);
+
+    a = false;
+    b = true;
+
+    result = a || b;
+
+    printf("a = %d, b = %d\n", a, b);
+    printf("The result of a || b = %d\n", result);
+
+    printf("\nAND (&&) Operations.\n");
+    a = true;
+    b = true;
+
     result = a && b;
 
-    printf("%d\n", result);
+    printf("a = %d, b = %d\n", a, b);
+    printf("The result of a && b = %d\n", result);
+
+    a = true;
+    b = false;
+
+    result = a && b;
+
+    printf("a = %d, b = %d\n", a, b);
+    printf("The result of a && b = %d\n", result);
+
+    a = false;
+    b = false;
+
+    result = a && b;
+
+    printf("a = %d, b = %d\n", a, b);
+    printf("The result of a && b = %d\n", result);
+
+    a = false;
+    b = true;
+
+    result = a && b;
+
+    printf("a = %d, b = %d\n", a, b);
+    printf("The result of a && b = %d\n", result);
+
     return 0;
 }
 
